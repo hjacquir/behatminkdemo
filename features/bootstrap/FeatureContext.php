e021778fa7a73d542d3ebdf07196c67d4da93073
hjacquir <jacquirhatim@gmail.com>
2015-02-23 22:39:19 +0100

    selenium server and sanitize code

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,45 +1,18 @@
 <?php
 
-use Behat\Behat\Context\ClosuredContextInterface,
-    Behat\Behat\Context\TranslatedContextInterface,
-    Behat\Behat\Context\BehatContext,
-    Behat\Behat\Exception\PendingException;
-use Behat\Gherkin\Node\PyStringNode,
-    Behat\Gherkin\Node\TableNode;
-
-//
-// Require 3rd-party libraries here:
-//
-//   require_once 'PHPUnit/Autoload.php';
-//   require_once 'PHPUnit/Framework/Assert/Functions.php';
-//
+use Behat\MinkExtension\Context\MinkContext;
 
 /**
  * Features context.
  */
-//class FeatureContext extends BehatContext
-class FeatureContext extends \Behat\MinkExtension\Context\MinkContext
+class FeatureContext extends MinkContext
 {
     /**
-     * Initializes context.
-     * Every scenario gets it's own context object.
-     *
-     * @param array $parameters context parameters (set them up through behat.yml)
+     * @When /^I search for "([^"]*)"$/
      */
-    public function __construct(array $parameters)
+    public function iSearchFor($arg1)
     {
-        // Initialize your context here
+        $this->fillField('Search Drupal.org', $arg1);
+        $this->pressButton('Search');
     }
-
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        doSomethingWith($argument);
-//    }
-//
 }
--- features/test.feature
+++ features/test.feature
@@ -1,11 +1,16 @@
+@javascript
 Feature: Searching on Drupal.org
   In order to find module on Drupal.org
   As an drupal user
   I need to be able to perform a research on Drupal.org
 
-  @javascript
-  Scenario: Searching Behat on Drupal.org
+  Scenario: Searching Behat on Drupal.org method 1
     Given I go to "http://drupal.org"
     When I fill in "Search Drupal.org" with "behat"
     And I press "Search"
+    Then I should see "Behat Drupal Extension"
+
+  Scenario: Searching for Behat on Drupal.org method 2
+    Given I go to "http://drupal.org"
+    When I search for "behat"
     Then I should see "Behat Drupal Extension"
